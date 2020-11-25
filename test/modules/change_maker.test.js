85a379f5279bcde93878fb8af1277914c9dae350
Keller Martin <kmartin@appriss.com>
2020-11-25 12:01:24 -0500

    FINISHED

---
+++ test/modules/change_maker.test.js
@@ -0,0 +1,27 @@
+const makeChange = require('../../src/modules/change_maker');
+
+describe('changeMaker', () => {
+  const COINS = [25, 10, 5, 1];
+
+  it('when 80', () => {
+    let expected = {
+      '25': 3,
+      '10': 0,
+      '5': 1,
+      '1': 0
+    };
+
+    expect(makeChange(80, COINS)).toEqual(expected)
+  });
+
+  it('when 93', () => {
+    let expected = {
+      '25': 3,
+      '10': 1,
+      '5': 1,
+      '1': 3
+    };
+
+    expect(makeChange(93, COINS)).toEqual(expected)
+  })
+});
